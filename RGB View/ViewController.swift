0133b737c5fc2cb9223062a523aa952a1d8afd96
amaya saudade <iwaizumi.yoshi@gmail.com>
2024-02-04 21:35:19 +0300

    add interface elements

--- RGB View/ViewController.swift
+++ RGB View/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 10
     }
 
 
